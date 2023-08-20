3e906c6d4da1ef72c9fb95285078199d37e9c80f
henriqueAvner <avnerhdpb@gmail.com>
2023-08-20 17:46:10 -0300

    Feat: Requisito 4 - Validando os campos do formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,26 +2,70 @@ import { useState } from 'react';
 
 export default function Form() {
   const [ocultForm, setOcultForm] = useState(true);
+  const [serviceName, setServiceName] = useState('');
+  const [login, setLogin] = useState('');
+  const [thisPassword, setThisPassword] = useState('');
+  const [link, setLink] = useState('');
+  const [disableButton, setdisabbleButton] = useState(true);
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+  const testPassword = () => {
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,16}$/;
+    return regex.test(thisPassword);
+  };
+
+  function handleForm() {
+    if (serviceName !== ''
+    && login !== ''
+    && thisPassword !== ''
+    && thisPassword.length >= 8 && thisPassword.length <= 16
+    && link !== ''
+    && testPassword()) {
+      setdisabbleButton(false);
+    } else {
+      setdisabbleButton(true);
+    }
+  }
+
   return (
     <div>
       {!ocultForm && (
-        <form>
+        <form onSubmit={ handleSubmit } onChange={ handleForm }>
           <label>
             Nome do serviço
-            <input type="text" />
+            <input
+              type="text"
+              value={ serviceName }
+              onChange={ (event) => setServiceName(event.target.value) }
+            />
           </label>
           <label>
             Login
-            <input type="text" />
+            <input
+              type="text"
+              value={ login }
+              onChange={ (event) => setLogin(event.target.value) }
+            />
           </label>
           <label>
             Senha
-            <input type="password" />
+            <input
+              type="password"
+              value={ thisPassword }
+              onChange={ (event) => setThisPassword(event.target.value) }
+            />
           </label>
           <label>
             URL
-            <input type="text" />
-            <button>Cadastrar</button>
+            <input
+              type="text"
+              value={ link }
+              onChange={ ({ target }) => setLink(target.value) }
+            />
+            <button disabled={ disableButton }>
+              Cadastrar
+            </button>
             <button onClick={ () => { setOcultForm(!ocultForm); } }>Cancelar</button>
           </label>
 
